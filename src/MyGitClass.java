6d4118890d8f622705e2fad96fd931f990eab997
pmelifge <109210047+pmelifge@users.noreply.github.com>
2022-07-16 22:47:03 +0300

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -4,5 +4,7 @@ public class MyGitClass {
     public static void main(String[] args) {
 
         System.out.println("I am learning GitHub!!");
+        System.out.println("I am adding new code...");
+        System.out.println("I am adding one more");
     }
 }
